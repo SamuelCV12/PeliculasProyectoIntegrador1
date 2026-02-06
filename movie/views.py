755de7bf175846a911e46173f421a7043b90f36d
malaura_t <mltafurg@eafit.edu.co>
2026-02-05 21:46:29 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'home.html', {'name': 'Greg Lim'})
+    #return HttpResponse('<h1>Welcome to the Movie Reviews Home Page!</h1>')
+# Create your views here.
+ #return render(request, 'home.html')
+# el render localiza el archivo, procesa el html 
+#y lo mete dentro de un Httpresponse que el mismo crea
+# entrega ese paquete hacia el navegador del usuario
