a0f10f458ec124eb88edfa2b4fc24a192f190f61
malaura_t <mltafurg@eafit.edu.co>
2026-02-05 23:15:56 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html', {'name': 'Greg Lim'})
+    return render(request, 'home.html', {'name': 'Maria Laura Tafur'})
     #return HttpResponse('<h1>Welcome to the Movie Reviews Home Page!</h1>')
 # Create your views here.
  #return render(request, 'home.html')
